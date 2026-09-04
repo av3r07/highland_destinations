51cce35f5495c7ec859ef26abb35acfe22b42108
av3r07 <abhishek07verma.av@gmail.com>
2026-09-04 23:32:24 +0530

    Replaced turbopack with webpack

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  sassOptions: {
-    additionalData: `@use "@styles/theme.scss" as *;`,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
